4a4b68b22afd77ae5a49f5295674f1fc18a66d98
pattyheiden <patriciasilvalimah@gmail.com>
2023-01-25 17:09:08 -0300

    modal, popover and progressbar

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,20 @@
+interface ProgressBarProps {
+    progress: number
+}
+
+export function ProgressBar(props: ProgressBarProps) {
+    const progressStyles = {
+        width: `${props.progress}%`
+    }
+    return (
+        <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+                        <div 
+                            className='h-3 rounded-xl bg-violet-600'
+                            style={progressStyles}
+                            role="progressbar"
+                            aria-label='Progresso de hábitos completados nesse dia.'
+                            aria-aria-valuenow={props.progress}
+                        />
+                    </div>
+    )
+}
